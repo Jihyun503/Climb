101dcbcbf58108ed503d8b779ebca04f8304c1bd
hyuni <wlgus7313@gmail.com>
2024-08-02 01:34:31 +0900

    fix: 일일권 목록 조회 엔드포인트 변경

--- climb-app/visit/urls/v1.py
+++ climb-app/visit/urls/v1.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from visit.views import v1
 
-visit = v1.VisitViewSet.as_view({"get": "list", "post": "create"})
+visit = v1.UserVisitViewSet.as_view({"post": "create"})
+visit_list = v1.VisitViewSet.as_view({"get": "list"})
 
 urlpatterns = [
     path("", visit, name="visit"),
+    path("list/", visit_list, name="visit_list"),
 ]
